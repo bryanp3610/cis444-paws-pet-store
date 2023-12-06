885afb5995bdd26bd5e1606192a798645b5d352c
tsmith311 <smith712@csusm.edu>
2023-12-05 21:01:43 -0800

    need to work on when adding user to table

--- groupproj/php/signUp.php
+++ groupproj/php/signUp.php
@@ -3,45 +3,42 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>signup</title>
 </head>
 <body>
 <?php
 
-ini_set('session.use_cookies','0');
-ini_set('session.use_only_cookies','0');
-
-session_start();
 //check if any input is empty
-
-
-
+if(empty($_GET['firstname']) || empty($_GET['lastname']) || empty($_GET['email']) || empty($_GET['password']))
+  exit("<p> Please enter information in all the input fields ! </p>");
 
 require_once("config.php");
 
 $TableName = "users";
-$firstName =$_GET['firstname'];
-$lastName=$_GET['lastname'];
-$Email=$_GET['email'];
-$passWord=$_GET['password'];
+$firstname =$_GET['firstname'];
+$lastname=$_GET['lastname'];
+$email=$_GET['email'];
+$password=$_GET['password'];
 
 
 $sql="SELECT * FROM $TableName"; //check if user registered already
 $result = pdo->query($sql);
 while($row = $result->fetch()){
-  if($row[''] ==$Email)
+  if($row['email'] ==$email)
   exit("<p> The email you entered is already in use. Please use another.</p>");
 }
 
-//if user isnt registered, add them to the table refer to register flyer.php flyer3
+//if user isnt registered, add them to the table 
   $sql="INSERT INTO $TableName ();
   $result = $pdo->prepare($sql);
-  $result->execute(array(":em"=>$Email,":ps"=>$passWord));
+  $result->execute(array(":em"=>$email,":ps"=>$password));
   $row =$result->fetch();
   $userID=$row['userID'];
   setcookie("userID",$userID); //setting cookie now
 
-  $pdo =null;
+  $pdo = null; //close connection
 ?>
+
+
 </body>
 </html>
