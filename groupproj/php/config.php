6732fbcfca9b8d8e655fce68b7732483342a4870
tsmith311 <smith712@csusm.edu>
2023-12-08 13:11:11 -0800

    user gets added to table

--- groupproj/php/config.php
+++ groupproj/php/config.php
@@ -1,11 +1,29 @@
 <?php
-
-  $connString = "mysql:host = localhost; dbname =PawsPetStore";
-  $user ="pawspets";
-  $pass="pawesome";
+/*
+  $connString = "mysql:host=localhost;dbname =PawsPetsStore";
+  $user ="root";
+  $pass="root";
 
   $pdo = new pdo($connString,$user,$pass);
+  
+ try {
+    $pdo = new PDO($connString, $user, $pass);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
   $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+*/
+
+
+$connString = "mysql:host=localhost; dbname=PawsPetStore";
+$user="root";
+$pass="root";
+
+$pdo=new pdo($connString,$user,$pass);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+
 
 ?>
--- groupproj/php/signUp.php
+++ groupproj/php/signUp.php
@@ -1,44 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>signup</title>
-</head>
-<body>
 <?php
 
 //check if any input is empty
 if(empty($_GET['firstname']) || empty($_GET['lastname']) || empty($_GET['email']) || empty($_GET['password']))
   exit("<p> Please enter information in all the input fields ! </p>");
 
+
 require_once("config.php");
 
 $TableName = "users";
-$firstname =$_GET['firstname'];
-$lastname=$_GET['lastname'];
+$firstName =$_GET['firstname'];
+$lastName=$_GET['lastname'];
 $email=$_GET['email'];
 $password=$_GET['password'];
 
+echo($email);
+echo($password);
+echo($firstName);
+echo($lastName);
 
 $sql="SELECT * FROM $TableName"; //check if user registered already
-$result = pdo->query($sql);
+$result = $pdo->query($sql);
 while($row = $result->fetch()){
   if($row['email'] ==$email)
   exit("<p> The email you entered is already in use. Please use another.</p>");
 }
+//insert user into table
 
-//if user isnt registered, add them to the table 
-  $sql="INSERT INTO $TableName ();
+  $sql="INSERT INTO $TableName VALUES(NULL,:fn,:ln, :em,:ps)";
   $result = $pdo->prepare($sql);
-  $result->execute(array(":em"=>$email,":ps"=>$password));
+  $result->execute(array(":fn"=>$firstName, ":ln"=>$lastName, ":em"=>$email,":ps"=>$password));
+
   $row =$result->fetch();
   $userID=$row['userID'];
+ // $_SESSION['userID']=$userID;
   setcookie("userID",$userID); //setting cookie now
 
   $pdo = null; //close connection
+
 ?>
 
 
-</body>
-</html>
+
+
+
+
+
